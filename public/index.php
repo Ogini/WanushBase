69107cbbeda27a7b19fdc25b983273bd1e4a2f2f
Michael Wanush <Michael.Wanush@sunzinet.com>
2016-08-10 16:30:59 +0200

    Initial Commit'

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * index.php
+ * Date: 10.08.2016
+ * Time: 16:23
+ * PHP version 5
+ *
+ * @author    Michael Wanush <michael.wanush@sunzinet.com>
+ * @copyright 2016 sunzinet AG
+ */
+
+require __DIR__ . '../src/Bootstrap.php';
