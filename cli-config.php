0377044bae6f30ba7f3a555a811709ad9fb96430
Michael Wanush <Michael.Wanush@sunzinet.com>
2016-12-07 15:15:20 +0100

    Remove doctrine vestiges

--- cli-config.php
+++
@@ -1,7 +0,0 @@
-<?php
-use Doctrine\ORM\Tools\Console\ConsoleRunner;
-
-// replace with file to your own project bootstrap
-require_once __DIR__ . '/src/Bootstrap.php';
-
-return ConsoleRunner::createHelperSet($entityManager);
